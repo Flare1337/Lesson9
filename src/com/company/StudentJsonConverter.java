37d8c4a0a6da113aaa471146ab2680b918c10539
Flare1337 <dimonhek1234567@mail.ru>
2021-10-24 19:12:51 +0300

    JSONv1

--- src/com/company/StudentJsonConverter.java
+++ src/com/company/StudentJsonConverter.java
@@ -3,6 +3,12 @@ package com.company;
 public class StudentJsonConverter implements IStudentStringConverter {
     @Override
     public String convert(Student student) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{\n");
+        stringBuilder.append("\"name\":").append("\"" + student.getName()).append(",\n");
+        stringBuilder.append("\"age\":").append(student.getAge()).append(",\n");
+        stringBuilder.append("\"salary\":").append(student.getSalary()).append("\n");
+        stringBuilder.append("\n}");
+        return stringBuilder.toString();
     }
 }
--- src/com/company/StudentKeyValueConverter.java
+++ src/com/company/StudentKeyValueConverter.java
@@ -4,6 +4,7 @@ public class StudentKeyValueConverter implements IStudentStringConverter {
     @Override
     public String convert(Student student) {
         StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{\n");
         stringBuilder.append("name=").append(student.getName()).append("\n");
         stringBuilder.append("age=").append(student.getAge()).append("\n");
         stringBuilder.append("salary=").append(student.getSalary()).append("\n");
