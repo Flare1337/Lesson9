87b298731bbb82a4da904093cffa429d1217df19
Flare1337 <dimonhek1234567@mail.ru>
2021-11-21 15:35:39 +0200

    Repaired according to the feedback

--- src/com/company/StudentToStringPrinter.java
+++ src/com/company/StudentToStringPrinter.java
@@ -7,8 +7,6 @@ public class StudentToStringPrinter implements IStudentPrinter {
         this.converter = converter;
     }
 
-    // В задании указано что мы в любом случае в любом случае превращаем студента в строку
-    // так что, превращаем переопределенным методом а остальное как описано в ТЗ, ничего не делаем
     @Override
     public void print(Student student) {
         converter.convert(student);
